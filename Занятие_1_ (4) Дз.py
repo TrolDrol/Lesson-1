23a2d688ecfe38e0aeb322e6934142aa0fdc4eb3
Азебан <yushurup@gmail.com>
2023-06-20 22:31:05 +0300

    'slice'

--- Занятие_1_ (4) Дз.py
+++ Занятие_1_ (4) Дз.py
@@ -1,11 +1,19 @@
-def polindrom(clovo):
-    adict = [clovo]
-    adict2 = []
-    adict2.append(clovo[::-1])
-    if adict == adict2:
-        print(True)
+# def is_polindrom(word):
+#     adict = [word]
+#     adict2 = []
+#     adict2.append(word[::-1])
+#     if adict == adict2:
+#         return True
+#     else:
+#         return False
+#
+# word = input(f'Введите слово: ')
+# print(is_polindrom(word)) # Решил оставить и 1 вариант для себя
+def is_polindrom(word):
+    if word == word[::-1]:
+        return True
     else:
-        print(False)
+        return False
 
-clovo = input(f'Введите слово: ')
-polindrom(clovo)
+word = input(f'Введите слово: ')
+print(is_polindrom(word))
